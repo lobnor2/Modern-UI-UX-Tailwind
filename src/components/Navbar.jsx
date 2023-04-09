712b72147a394c3334cfa9c40be1151fedb39e85
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-04-09 21:58:23 +0530

    added mobile responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center border border-white my-1">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
@@ -22,7 +22,34 @@ const Navbar = () => {
       </ul>
       {/* ul only for mobile view */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img src={menu} alt="" />
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          //thats not the right way to do it, in react you should never update the state  using prev version of the same old state
+          // onClick={() => setToggle(!toggle)}
+
+          //thats the right way, it will make sure that keeps state up to date
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl  sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1 ">
+            {navLinks.map((nav, index) => (
+              <li
+                key={index}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
