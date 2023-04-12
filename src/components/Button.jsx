a90ec4c927da0b6fa2c36a8652abc5dd7e89fcb0
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-04-12 07:48:35 +0530

    button component created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button "
+      className={`py-4 px-6 bg-blue-gradient text-black mt-2 font-poppins font-semibold text-[18px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
