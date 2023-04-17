1b09484dd8ef0cef0c3a7c61988828be091e849f
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-04-17 07:45:26 +0530

    done card deal component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button "
-      className={`py-4 px-6 bg-blue-gradient text-black mt-2 font-poppins font-semibold text-[18px] outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient text-black mt-2 font-poppins font-semibold text-[18px] outline-none ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
